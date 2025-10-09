d01cd6f6fe0b83acc310fe1e26e0c95a136a749d
Jesus Chacon <chjesussao@gmail.com>
2025-10-08 20:45:14 -0400

    configuracion de vite y .env

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+
   const APP_PORT = Number(env.VITE_APP_BASE_PORT)
+  const APP_OPEN = Boolean(env.VITE_APP_BASE_OPEN)
+  const APP_HOST = Boolean(env.VITE_APP_BASE_HOST)
+  const APP_STRICT_PORT = Boolean(env.VITE_APP_BASE_STRICT_PORT)
+
+  const APP_GLOBAL = 'window'
 
   return {
-    server: { open: true, port: APP_PORT, host: true },
-    define: { global: 'window' },
+    server: {
+      open: APP_OPEN,
+      port: APP_PORT,
+      host: APP_HOST,
+      strictPort: APP_STRICT_PORT,
+    },
+    define: { global: APP_GLOBAL },
     resolve: {
       alias: [
         { find: '@', replacement: resolve(__dirname, 'src') },
