eee6ab158eab335ebb455789aa11012a3200afb8
Jesus Chacon <chjesussao@gmail.com>
2025-10-14 23:36:02 -0400

    correcion de rutas

--- src/routes/dashboardRoutes.tsx
+++ src/routes/dashboardRoutes.tsx
@@ -5,25 +5,21 @@ import { lazy } from 'react'
 import DashboardLayout from '@/components/layout/Dashboard'
 const ServicesPage = Loadable(lazy(() => import('@pages/ServicesPage')))
 const SearchDetailPage = Loadable(lazy(() => import('@pages/SearchDetailPage')))
-const ServiceDetailPage = Loadable(lazy(() => import('@pages/ServiceDetailPage')))
+const ServiceDetailPage = Loadable(
+  lazy(() => import('@pages/ServiceDetailPage'))
+)
 const CartPage = Loadable(lazy(() => import('@pages/CartPage')))
 
 const dashboardRoutes = {
-  path: '/',
+  path: '/app',
   element: <DashboardLayout />,
   children: [
-    {
-      path: '',
-      children: [
-        { index: true, element: <Navigate to="services" replace /> },
-        { path: '/services', element: <ServicesPage /> },
-        { path: '/search', element: <SearchDetailPage /> },
-        { path: '/search/:text', element: <SearchDetailPage /> },
-        { path: '/services/:id', element: <ServiceDetailPage /> },
-        { path: '/cart', element: <CartPage /> },
-        
-      ]
-    }
+    { index: true, element: <Navigate to="services" replace /> },
+    { path: 'services', element: <ServicesPage /> },
+    { path: 'search', element: <SearchDetailPage /> },
+    { path: 'search/:text', element: <SearchDetailPage /> },
+    { path: 'services/:id', element: <ServiceDetailPage /> },
+    { path: 'cart', element: <CartPage /> },
   ],
 }
 
