307106f22807cef6cebb2ac345ac3040aa8120d5
Jesus Chacon <chjesussao@gmail.com>
2025-10-16 22:20:19 -0400

    correcion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,33 +3,33 @@ import { useCartStore } from '../store/cartStore'
 import { useState } from 'react'
 
 function Home() {
-  const { items, addItem } = useCartStore();
-  const [showCart, setShowCart] = useState(true);
+  const { items, addItem } = useCartStore()
+  const [showCart, setShowCart] = useState(true)
 
   const handleTest = () => {
-    addItem({ 
-      id: '1', 
-      name: 'Articulo de prueba', 
+    addItem({
+      id: '1',
+      name: 'Articulo de prueba',
       quantity: 1,
       price: 150000,
       currency: 'COP',
       providerName: 'Proveedor de prueba',
-      image: '/service-image.jpg'
-    });
-    setShowCart(true);
-  };
+      image: '/service-image.jpg',
+    })
+    setShowCart(true)
+  }
 
   return (
     <Fragment>
       <h1>Home Page</h1>
       <h2>
         {showCart && items.length > 0
-          ? items.map(item => `${item.name} x${item.quantity}`).join(', ')
+          ? items.map((item) => `${item.name} x${item.quantity}`).join(', ')
           : ''}
       </h2>
       <button onClick={handleTest}>test</button>
     </Fragment>
-  );
+  )
 }
 
 export default Home
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,6 @@ import { createBrowserRouter } from 'react-router'
 import dashboardRoutes from '@/routes/dashboardRoutes'
 import authRoutes from '@/routes/authRoutes'
 
-
-
 const router = createBrowserRouter([
   { path: '*', element: <div>ERROR 404</div> },
   dashboardRoutes,
